9e700d3aaf860c8253c8f20f54c94e2ece3acf14
Aryan Sethi <aryan.rdps@gmail.com>
2024-12-28 22:44:40 +0530

    Added flapimage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import FlapImage from "./Components/FlapImage";
 
 function App() {
     return (
-        <div className="App">
+        <div
+            className="App"
+            style={{
+                background: "#000",
+                minHeight: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }}
+        >
             <h1>Hello World</h1>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "50px"
+                }}
+            >
+                <FlapImage />
+            </div>
         </div>
     );
 }
---
+++ src/Components/FlapImage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FlapImage.css";
+
+const FlapImage = () => {
+    return <div className="outer"></div>;
+};
+
+export default FlapImage;
